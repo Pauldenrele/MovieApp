90e56b2ca8b911ed873579a56655d532401d2157
PAULI <adenreleademola@yahoo.com>
2020-05-25 04:17:03 +0100

    Initial commit

---
+++ app/src/main/java/com/example/movieapp/SingleMovie/SingleMovieActivity.kt
@@ -0,0 +1,16 @@
+package com.example.movieapp.SingleMovie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.movieapp.R
+
+class SingleMovieActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_movie)
+
+
+
+    }
+}
